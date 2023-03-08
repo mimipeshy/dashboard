47c942ccbb0d70fe7a44f4669aa1b27cba90177b
mimipeshy <perisndanu@gmail.com>
2023-03-08 12:53:39 +0300

    complete topbar setup

--- src/scenes/global/topbar.jsx
+++ src/scenes/global/topbar.jsx
@@ -23,8 +23,29 @@ const Topbar = () =>{
             borderRadius="3px"
             >
                 <InputBase sx={{ml:2 , flex:1}} placeholder="search"/>
-
+                <IconButton type="button" sx={{p:1}}>
+                    <SearchIcon/>
+                </IconButton>
             </Box>
+            {/*ICONS*/}
+           <Box display="flex">
+            <IconButton onClick={colorMode.toggleColorMode}>
+                {theme.palette.mode === "dark" ? (
+                    <DarkModeOutlinedIcon/>
+                ): (
+               <LightModeOutlinedIcon/> 
+               )}
+            </IconButton>
+            <IconButton>
+                <NotificationsModeOutlinedIcon/>
+            </IconButton>
+            <IconButton>
+                <SettingsOutlinedIcon/>                
+            </IconButton>
+            <IconButton>
+                <PersonOutlinedIcon/>                
+            </IconButton>
+           </Box>
         </Box>
     )
 }

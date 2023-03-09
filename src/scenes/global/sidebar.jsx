9413a7f765ca052e08b76eaa8d64f73a5b4d03a1
mimipeshy <perisndanu@gmail.com>
2023-03-09 22:34:58 +0300

    design sidebar

--- src/scenes/global/sidebar.jsx
+++ src/scenes/global/sidebar.jsx
@@ -20,8 +20,30 @@ import MapOutlinedIcon from "@mui/icons-material/MapOutlined";
 const Sidebar = () =>{
     const theme = useTheme();
     const colors = tokens(theme.palette.mode);
-    const [isCollapsed, setIsCollpsed] = useState(false);
-    return <div>Sidebar</div>
+    const [isCollapsed, setIsCollapsed] = useState(false);
+    const [selected, setSelected] = useState("Dashboard")
+    return 
+    (
+    <Box
+    sx={{
+        "& .pro-sidebar-inner": {
+            background: `${colors.primary[400]} !important`,
+          },
+          "& .pro-icon-wrapper": {
+            backgroundColor: "transparent !important",
+          },
+          "& .pro-inner-item": {
+            padding: "5px 35px 5px 20px !important",
+          },
+          "& .pro-inner-item:hover": {
+            color: "#868dfb !important",
+          },
+          "& .pro-menu-item.active": {
+            color: "#6870fa !important",
+          },
+    }}
+    ></Box>
+    )
 }
 
 export default Sidebar;

a6e366259b64af3451cf7bc8e6a8ac6bf5ea38d6
mimipeshy <perisndanu@gmail.com>
2023-03-09 22:31:52 +0300

    design sidebar

--- src/scenes/global/sidebar.jsx
+++ src/scenes/global/sidebar.jsx
@@ -4,7 +4,6 @@ import 'react-pro-sidebar/dist/css/styles.css';
 import {Box, IconButton, Typography, useTheme} from "@mui/material";
 import { Link } from "react-router-dom";
 import { tokens } from "../../theme";
-
 import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
 import PeopleOutlinedIcon from "@mui/icons-material/PeopleOutlined";
 import ContactsOutlinedIcon from "@mui/icons-material/ContactsOutlined";
@@ -19,6 +18,9 @@ import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
 import MapOutlinedIcon from "@mui/icons-material/MapOutlined";
 
 const Sidebar = () =>{
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+    const [isCollapsed, setIsCollpsed] = useState(false);
     return <div>Sidebar</div>
 }
 

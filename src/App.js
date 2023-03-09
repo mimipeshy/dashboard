5e197699a3648cc2ecfcdbcc93c4ab7c1f5fd5cc
mimipeshy <perisndanu@gmail.com>
2023-03-09 22:13:00 +0300

    setup routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { ColorModeContext, useMode} from "./theme";
 import { CssBaseline, ThemeProvider } from "@mui/material";
-import Topbar from "./scenes/global/topbar";
+import { Routes, Route } from "react-router-dom";
+
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -11,6 +12,19 @@ function App() {
       <div className="app">
         <main className="content">
           <Topbar/>
+          <Routes>
+          <Route path="/" element={<Dashboard />} />
+              <Route path="/team" element={<Team />} />
+              <Route path="/contacts" element={<Contacts />} />
+              <Route path="/invoices" element={<Invoices />} />
+              <Route path="/form" element={<Form />} />
+              <Route path="/bar" element={<Bar />} />
+              <Route path="/pie" element={<Pie />} />
+              <Route path="/line" element={<Line />} />
+              <Route path="/faq" element={<FAQ />} />
+              <Route path="/calendar" element={<Calendar />} />
+              <Route path="/geography" element={<Geography />} />
+          </Routes>
         </main>
       </div>
     </ThemeProvider>

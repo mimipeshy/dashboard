5b9383434981b26454e0e79ffb799c8cfb54a021
mimipeshy <perisndanu@gmail.com>
2023-03-16 16:06:25 +0300

    design header

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -1,5 +1,17 @@
-const Dashboard = () =>{
-    return <div>Dashboard yangu</div>
-}
+import Header from "../../components/Header";
+import { Box } from "@mui/material";
+
+const Dashboard = () => {
+    return (
+        <Box m="20px">
+            <Box
+                display="flex"
+                justifyContent="space-between"
+                alignItems="center">
+                <Header title="DASHBOARD" subtitle="Welcome to your dashboard" />
+            </Box>
+        </Box>
+    );
+};
 
 export default Dashboard;

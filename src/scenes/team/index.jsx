282c045c6e9185589ba4b961860fc5ae71105f11
mimipeshy <perisndanu@gmail.com>
2023-03-16 17:04:39 +0300

    design team

--- src/scenes/team/index.jsx
+++ src/scenes/team/index.jsx
@@ -72,7 +72,32 @@ const Team = () => {
             <Header title="TEAM" subtitle="Managing team members" />
             <Box 
             m="40px 0 0 0"
-            height="75vh">
+            height="75vh"
+            sx={{
+                "& .MuiDataGrid-root": {
+                  border: "none",
+                },
+                "& .MuiDataGrid-cell": {
+                  borderBottom: "none",
+                },
+                "& .name-column--cell": {
+                  color: colors.greenAccent[300],
+                },
+                "& .MuiDataGrid-columnHeaders": {
+                  backgroundColor: colors.blueAccent[700],
+                  borderBottom: "none",
+                },
+                "& .MuiDataGrid-virtualScroller": {
+                  backgroundColor: colors.primary[400],
+                },
+                "& .MuiDataGrid-footerContainer": {
+                  borderTop: "none",
+                  backgroundColor: colors.blueAccent[700],
+                },
+                "& .MuiCheckbox-root": {
+                  color: `${colors.greenAccent[200]} !important`,
+                },
+              }}>
                 <DataGrid
                     rows={mockDataTeam}
                     columns={columns}

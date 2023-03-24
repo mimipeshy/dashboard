0fd56c885025b809bf3b611e2cf45d37afa3765f
mimipeshy <perisndanu@gmail.com>
2023-03-24 09:47:59 +0300

    add user reg form

--- src/scenes/form/index.jsx
+++ src/scenes/form/index.jsx
@@ -127,6 +127,9 @@ const Form = () => {
     );
 };
 
+const phoneRegExp =
+  /^((\+[1-9]{1,4}[ -]?)|(\([0-9]{2,3}\)[ -]?)|([0-9]{2,4})[ -]?)*?[0-9]{3,4}[ -]?[0-9]{3,4}$/;
+
 const checkoutSchema = yup.object().shape({
     firstName: yup.string().required("required"),
     lastName: yup.string().required("required"),
